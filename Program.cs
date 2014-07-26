fdaeff1e75a4cc8a15eab2b2212da65a6cdf9499
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-26 22:17:45 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thrift_discovery_service_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
