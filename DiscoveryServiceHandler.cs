f157ccd981c7bc93cb66e8e27ba95261176cf83c
VitaliyOliynyk <xaoc00@gmail.com>
2014-07-26 23:02:18 +0200

    add server code

---
+++ DiscoveryServiceHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using thrift.discoveryservice;
+
+namespace thrift_discovery_service_csharp
+{
+    public class DiscoveryServiceHandler : thrift.discoveryservice.DiscoveryService.Iface
+    {
+        
+        public ServiceInfo getInfo(string serviceName)
+        {
+            switch (serviceName)
+            {
+                case "calculator":
+                    return buildServiceInfo("localhost", 10002);
+            }
+            ServiceNotFoundException serviceNotFoundException = new ServiceNotFoundException();
+            serviceNotFoundException.Message = "Service '" + serviceName + "' not found"; 
+            throw serviceNotFoundException;
+        }
+
+        private ServiceInfo buildServiceInfo(string host, short port)
+        {
+            ServiceInfo serviceInfo = new ServiceInfo();
+            serviceInfo.Host = host;
+            serviceInfo.Port = port;
+            return serviceInfo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using thrift.discoveryservice;
+using Thrift.Transport;
+using Thrift.Server;
 
 namespace thrift_discovery_service_csharp
 {
@@ -10,6 +13,38 @@ namespace thrift_discovery_service_csharp
     {
         static void Main(string[] args)
         {
+            short portNumber = 10001;
+            if (args.Length > 1)
+            {
+                syntax();
+                return;
+            }
+
+            if (args.Length == 1) 
+            {
+                try
+                {
+                    portNumber = short.Parse(args[0]);
+                }
+                catch (Exception)
+                {
+                    syntax();
+                    return;
+                }
+            }
+ 
+
+            DiscoveryService.Iface handler = new DiscoveryServiceHandler();
+            DiscoveryService.Processor processor = new DiscoveryService.Processor(handler);
+            TServerTransport serverTransport = new TServerSocket(portNumber);
+            TServer server = new TThreadPoolServer(processor, serverTransport);
+            Console.WriteLine("Starting the server...");
+            server.Serve();
+        }
+
+        private static void syntax()
+        {
+            Console.WriteLine("syntax: <program name> <port number>");
         }
     }
 }
